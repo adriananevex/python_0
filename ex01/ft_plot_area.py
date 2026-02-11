b97b18f24779b88e0a0cdffec86727ebb32eb946
Adriana Neves <aneves@c2r8s6.42lisboa.com>
2026-02-11 21:56:50 +0000

    python_0

---
+++ ex01/ft_plot_area.py
@@ -0,0 +1,7 @@
+def ft_plot_area():
+    length = int(input("Length: "))
+    width = int(input("Width: "))
+    area = length * width
+    print("Plot area:", area)
+
+ft_plot_area()
---
+++ ex02/ft_harvest_total.py
@@ -0,0 +1,8 @@
+def ft_harvest_total():
+    value_1 = int(input("Value 1: "))
+    value_2 = int(input("Value 2: "))
+    value_3 = int(input("Value 3: "))
+
+    total = value_1 + value_2 + value_3
+
+    print("Total harvest:", total)
